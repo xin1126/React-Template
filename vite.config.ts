421fd5a98c21ed31d38415458846c46359c80592
xin1126 <z26319491z@gmail.com>
2022-10-23 01:40:47 +0800

    feat: 設定@/路徑

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+import * as path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
