82224859b604145df525ccc68aa17f9495f786cb
xin1126 <z26319491z@gmail.com>
2022-10-23 01:41:22 +0800

    feat: tailwind.config 自訂顏色

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      // 自訂顏色
+      colors: {
+        primary: '#155e75',
+        secondary: '#6b21a8',
+        success: '#065f46',
+        warning: '#d97706',
+        danger: '#991b1b',
+      },
+    },
   },
   plugins: [],
 }
